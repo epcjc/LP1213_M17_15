0a21846ed1b5a5515f03e882ec058223ad686bdf
Tiago10 <i10755@epcjc.net>
2013-05-30 11:44:16 +0100

    Desenvolvimento do projeto

--- ProjetoFinal/ProjetoFinal/Listar.cs
+++ ProjetoFinal/ProjetoFinal/Listar.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace ProjetoFinal
 {
-    public partial class Form1 : Form
+    public partial class Listar : Form
     {
-        public Form1()
+        public Listar()
         {
             InitializeComponent();
         }
---
+++ ProjetoFinal/ProjetoFinal/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetoFinal
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void listarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Inserir frm = new Inserir();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eliminar frm = new Eliminar();
+            frm.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Listar frm = new Listar();
+            frm.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Alterar frm = new Alterar();
+            frm.ShowDialog();
+        }
+    }
+}
